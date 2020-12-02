7de5d4a44431e3040ac912034a96695ddbe50e58
Darren <48471361+Dazzamac@users.noreply.github.com>
2020-12-02 10:43:09 +0000

    Add files via upload

---
+++ AdventofCodeDay2_1.ps1
@@ -0,0 +1,16 @@
+﻿$PassFile = Get-Content .\Day2Input.txt
+[int]$GoodCount = 0   
+$PassFile | Foreach { 
+    $Range = $($_.Split(" ")[0])
+    $Char = $(($_.Split(" ")[1]).trim(":"))
+    $Password = $($_.Split(" ")[2])
+
+    [int]$Lower,[int]$Upper = $range.Split("-")
+    $CharCount = ($Password.ToCharArray() | Where-Object {$_ -eq $Char} | Measure-Object).Count
+    If ($Charcount -ge $Lower -and $charcount -le $upper) {
+        $GoodCount++
+        }
+    }
+
+$GoodCount
+
