61f6838267f5175fc29f2f033e1f7a405c1f3802
Darren <48471361+Dazzamac@users.noreply.github.com>
2020-12-04 11:29:50 +0000

    Add files via upload
    WIP code, works but wrong result

---
+++ AdventofCodeDay4_1.ps1
@@ -0,0 +1,31 @@
+﻿$PPData = Get-content .\Day4Input.txt
+$Passport =  @{}
+$Valid = 0
+$PPCount = 0
+
+$PPData | ForEach {
+    If ($_){
+        $_.Split(" ") | ForEach {
+            $Key,$Value = $_.Split(":")
+            $Passport.Add($key,$value)
+        }
+        If (($Passport.Count -eq 7 -and !($Passport.CID)) -or ($Passport.count -eq 8)){ 
+        $Valid ++
+        $Passport | out-file pass.txt -append
+        
+        }  
+        
+ #Blank Line, reset $Passport variable########        
+    } Else {
+        #If ($PPCount -gt 100) {Break}
+        
+        $Passport = @{}
+        $PPCount ++
+        
+    }
+
+        
+        
+  }
+
+  $Valid
