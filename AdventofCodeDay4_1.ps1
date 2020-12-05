c083aaf109b7ac73d2ff135f1a2749566b764df1
Darren <48471361+Dazzamac@users.noreply.github.com>
2020-12-05 11:29:40 +0000

    Add files via upload

--- AdventofCodeDay4_1.ps1
+++ AdventofCodeDay4_1.ps1
@@ -1,35 +1,40 @@
 ﻿$PPData = Get-content .\Day4Input.txt
 $Passport =  @{}
 $Valid = 0
+$PPCount = 0
+$badpass = 0
 
-
-$PPData | ForEach {#Iterate each line
-    If ($_){#Check if line is blank or not
-        $_.Split(" ") | ForEach { #Separate each item field and iterate
-            $Key,$Value = $_.Split(":")#Create Keypairs
-            $Passport.Add($key,$value)#Add keypairs to hash
-        }
-        If (($Passport.Count -eq 7 -and !($Passport.CID)) -or ($Passport.count -eq 8)){ #Check if all data is complete or at least only CID is missing
-        $Valid ++ #Update valid count
-        $Passport | out-file pass.txt -append #Add valid passport data to file for troubleshooting and verification
+$PPData | ForEach {
+    If ($_){
+        $_.Split(" ") | ForEach {
+            $Key,$Value = $_.Split(":")
+            $Passport.Add($key,$value)
+        }    
         
-        }  
-        
- #########Blank Line, reset $Passport variable########        
+ #Blank Line, reset $Passport variable########        
     } Else {
-                
+        
+        If ($Passport.byr -and`
+         $Passport.iyr -and`
+          $Passport.eyr -and`
+           $Passport.hgt -and`
+            $Passport.hcl -and`
+             $Passport.ecl -and`
+              $Passport.pid){ 
+           
+        $Valid ++
+        $Passport | Out-File Passport.txt -append
+
+        
+        }
         $Passport = @{}
-               
+        $PPCount ++
+        
     }
 
         
         
   }
 
-  $Valid # Return result
-  
-  If ($Passport.byr -and $Passport.iyr -and $Passport.eyr -and $Passport.hgt -and $Passport.hcl -and $Passport.ecl -and $Passport.pid <#-and $passport.cid#>){ 
-        $Valid ++
-
-        
-        }  
+  $Valid
+  $badpass
