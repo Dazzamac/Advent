e9f9463361f7d9bb4003246f092f841a4776a0bf
Darren <48471361+Dazzamac@users.noreply.github.com>
2020-12-08 10:51:33 +0000

    Add files via upload

---
+++ AdventOfCodeDay5_1.ps1
@@ -0,0 +1,38 @@
+﻿
+$BoardingCards = Get-content .\Day5Input.txt
+$HighseatID = 0
+$Allseats = @()
+
+
+$Boardingcards | ForEach {
+    $BoardingCard = $_.ToCharArray()
+    $Seats = @()
+    For ($x = 0; $x -le 7; $x++) {
+        $Seats += $x
+    }
+    $Rows = @()
+    For ($x = 0; $x -le 127; $x++) {
+        $Rows += $x
+    }
+    $BoardingCard | ForEach {
+        
+        $MidRowIndex = [int](($Rows.Length+1)/2)
+        $MidSeatIndex = [int](($Seats.Length+1)/2)
+        Switch ($_){
+            F {$Rows = $Rows[0..(([Math]::Floor($Rows.count)/2)-1)]}
+            B {$Rows = $Rows[([Math]::Ceiling($Rows.count/2))..($Rows.count-1)]}
+            L {$Seats = $Seats[0..(([Math]::Floor($Seats.count)/2)-1)]}
+            R {$Seats =$Seats[([Math]::Ceiling($Seats.count/2))..($Seats.Count-1)]}
+
+        }#Switch
+
+    }#BoardingCard | ForEach
+    #$Rows
+    #$Seats
+    $SeatID = ($($Rows) * 8 + $($Seats))
+    $seatID
+    $HighseatID = ([Math]::Max($SeatID,$HighseatID))
+    $HighseatID
+    $AllSeats += $SeatID
+
+}#$BoardingCards | ForEach 
