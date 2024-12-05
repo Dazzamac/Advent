14499c17cd33538ace9c76d40430ee21fe91e3cc
darrenmccann-payroc <Darren.mccann@payroc.com>
2024-12-05 15:29:30 +0000

    Day 2 Part 1 Complete

--- 2024/Solutions/Day2.ps1
+++ 2024/Solutions/Day2.ps1
@@ -30,15 +30,62 @@ So, in this example, 2 reports are safe.
 Analyze the unusual data from the engineers. How many reports are safe?#>
 
 # Load input data from a file
-$Inputs = Get-Content "./Inputs/Day2_Test.txt"
+$Inputs = Get-Content "./Inputs/Day2.txt"
 
 # Initialize a count of safe reports
 $SafeCount = 0
 
 # Iterate the reports
 ForEach ($Report in $Inputs) {
-    $Values = $Report -split '\s+'
+    $Values = $Report -split '\s+' | ForEach-Object { [int]$_}
     # Test 1: Any two adjacent levels differ at least one and less than 3
-    
+    $Safe = $true
+    For ($i=0;$i -lt ($Values.count -1); $i++){
+        $Difference = [math]::Abs($Values[$i] - $Values[$i+1])
+        #$Difference
+        If ($Difference -lt 1 -or $Difference -gt 3){
+            $Safe = $false
+            break
+        }
+    }
+    # Test 2: Check that values are increasing or decreasing
+    If ($Safe){
+        Write-Verbose "Test 1 Complete"
+
+        # Initialize flags to check for ascending or descending order
+        $Descending = $true
+        $Ascending = $true
+
+
+        For ($i=0;$i -lt ($Values.count -1); $i++){
+            # Check if the current number is greater than the next one (for descending order)
+            If ($Values[$i] -gt $Values[$i+1]){
+                $Ascending = $false
+            }
+            # Check if the current number is less than the next one (for ascending order)
+            if ($Values[$i] -lt $Values[$i + 1]) {
+                $Descending = $false
+            }
+        }
+        # Output the result based on the flags
+        if ($Ascending) {
+            Write-Verbose "Test 2 Complete: Ascending"
+            $SafeCount++
+        } elseif ($Descending) {
+            Write-Verbose "Test 2 Complete: Descending"
+            $SafeCount++
+        } else {
+            Write-Verbose "Test 2 Failed: Not in order"
+            $Safe = $False
+        }
+    } Else {
+        Write-Verbose "Test 1 Failed: Increments too large"
+    }
     
 }
+
+Write-Host "Total number of safe reports is: $Safecount"
+
+#########################################################################################
+#######################################Part 2############################################
+#########################################################################################
